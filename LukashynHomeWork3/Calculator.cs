09346603713cd3868584599a2a7992e8b5fcb443
Alex Lukashun <alexlukus@outlook.com>
2019-11-10 11:24:09 +0200

    Add project files.

---
+++ LukashynHomeWork3/Calculator.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LukashynHomeWork3
+{
+    class Calculator
+    {
+        public static List<char> ReadInput()
+        {
+            var input = new List<char>();
+            while (true)
+            {
+                for (int i = 0; ; i++)
+                {
+                    var t = Console.ReadKey();
+                    if ((t.KeyChar >= '0' && t.KeyChar <= '9') || t.KeyChar == '*' || t.KeyChar == '/' || t.KeyChar == '+' || t.KeyChar == '-' || t.KeyChar == '=')
+                    {
+                        input.Add(t.KeyChar);
+                        if (t.KeyChar == '=')
+                        {
+                            if (i >= 1 && input[i - 1] >= '0' && input[i - 1] <= '9')
+                            {
+                                return input;
+                            }
+                            else
+                            {
+                                throw new ArgumentException("Argument Exception. Incorrect use of =");
+                            }
+                        }
+                        if (t.KeyChar == '*' || t.KeyChar == '/' || t.KeyChar == '+' || t.KeyChar == '-')
+                        {
+                            if (i >= 1 && input[i - 1] >= '0' && input[i - 1] <= '9')
+                            {
+                            }
+                            else
+                            {
+                                throw new ArgumentException("Argument Exception. Incorrect use of operators");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Argument Exception. Unsupported symbol");
+                    }
+                }
+            }
+        }
+        public static void ParseInput(List<char> input, out List<int> numbers, out List<char> operators)
+        {
+            numbers = new List<int>();
+            operators = new List<char>();
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] == '*' || input[i] == '/' || input[i] == '+' || input[i] == '-')
+                {
+                    operators.Add(input[i]);
+                }
+                else if (input[i] == '=')
+                {
+
+                }
+                else
+                {
+                    if (i >= 1 && input[i - 1] >= '0' && input[i - 1] <= '9')
+                    {
+                        try
+                        {
+                            numbers[numbers.Count - 1] = int.Parse(numbers[numbers.Count - 1].ToString() + (input[i] - '0').ToString());
+                        }
+                        catch (OverflowException)
+                        {
+                            throw;
+                        }
+                    }
+                    else
+                    {
+                        numbers.Add(input[i] - '0');
+                    }
+                }
+            }
+        }
+        public static int Count(List<int> numbers, List<char> operators)
+        {
+            for (int i = 0; i < operators.Count; i++)
+            {
+                if (operators[i] == '*' || operators[i] == '/')
+                {
+
+                    if (operators[i] == '*')
+                    {
+                        try
+                        {
+                            checked
+                            {
+                                numbers[i] = numbers[i] * numbers[i + 1];
+                            }
+                            for (int j = i; j < operators.Count - 1; j++)
+                            {
+                                operators[j] = operators[j + 1];
+                                numbers[j + 1] = numbers[j + 2];
+                            }
+                            numbers.RemoveAt(numbers.Count - 1);
+                            operators.RemoveAt(operators.Count - 1);
+                            i--;
+                        }
+                        catch (OverflowException)
+                        {
+                            throw;
+                        }
+                    }
+                    else if (operators[i] == '/')
+                    {
+                        try
+                        {
+                            numbers[i] = numbers[i] / numbers[i + 1];
+                            for (int j = i; j < operators.Count - 1; j++)
+                            {
+                                operators[j] = operators[j + 1];
+                                numbers[j + 1] = numbers[j + 2];
+                            }
+                            numbers.RemoveAt(numbers.Count - 1);
+                            operators.RemoveAt(operators.Count - 1);
+                            i--;
+                        }
+                        catch (DivideByZeroException)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < operators.Count; i++)
+            {
+                if (operators[i] == '+')
+                {
+                    try
+                    {
+                        checked
+                        {
+                            numbers[i] = numbers[i] + numbers[i + 1];
+                        }
+                        for (int j = i; j < operators.Count - 1; j++)
+                        {
+                            operators[j] = operators[j + 1];
+                            numbers[j + 1] = numbers[j + 2];
+                        }
+                        numbers.RemoveAt(numbers.Count - 1);
+                        operators.RemoveAt(operators.Count - 1);
+                        i--;
+                    }
+                    catch (OverflowException)
+                    {
+                        throw;
+                    }
+                }
+                else if (operators[i] == '-')
+                {
+                    try
+                    {
+                        checked
+                        {
+                            numbers[i] = numbers[i] - numbers[i + 1];
+                        }
+                        for (int j = i; j < operators.Count - 1; j++)
+                        {
+                            operators[j] = operators[j + 1];
+                            numbers[j + 1] = numbers[j + 2];
+                        }
+                        numbers.RemoveAt(numbers.Count - 1);
+                        operators.RemoveAt(operators.Count - 1);
+                        i--;
+                    }
+                    catch (OverflowException)
+                    {
+                        throw;
+                    }
+                }
+            }
+            if (numbers.Count == 1)
+            {
+                return numbers[0];
+            }
+            else
+            {
+                throw new ArgumentException("Argument Exception. Something goes wrong");
+            }
+        }
+    }
+}
---
+++ LukashynHomeWork3/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LukashynHomeWork3
+{
+    public delegate int MyHandler(List<int> numbers, List<char> operators);
+    class Program
+    {
+        static void Main()
+        {
+            MyHandler myHandler;
+            Console.WriteLine("Solution maked with delegates(you can use +, -, *, /)");
+            while (true)
+            {
+                try
+                {
+                    var input = Calculator.ReadInput();
+                    Calculator.ParseInput(input, out List<int> numbers, out List<char> operators);
+                    myHandler = Calculator.Count;
+                    var answer = myHandler(numbers, operators);
+                    Console.WriteLine(answer);
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("Solution maked with anonymous methods(you can use only +, -)");
+            while (true)
+            {
+                try
+                {
+                    var input = Calculator.ReadInput();
+                    Calculator.ParseInput(input, out List<int> numbers, out List<char> operators);
+                    myHandler = delegate (List<int> aNumbers, List<char> aOperators)
+                    {
+                        for (int i = 0; i < aOperators.Count; i++)
+                        {
+                            switch (aOperators[i])
+                            {
+                                case '+':
+                                    try
+                                    {
+                                        checked
+                                        {
+                                            aNumbers[i] = aNumbers[i] + aNumbers[i + 1];
+                                        }
+                                        for (int j = i; j < aOperators.Count - 1; j++)
+                                        {
+                                            aOperators[j] = aOperators[j + 1];
+                                            aNumbers[j + 1] = aNumbers[j + 2];
+                                        }
+                                        aNumbers.RemoveAt(aNumbers.Count - 1);
+                                        aOperators.RemoveAt(aOperators.Count - 1);
+                                        i--;
+                                    }
+                                    catch (OverflowException)
+                                    {
+                                        throw;
+                                    }
+                                    break;
+                                case '-':
+                                    try
+                                    {
+                                        checked
+                                        {
+                                            aNumbers[i] = aNumbers[i] - aNumbers[i + 1];
+                                        }
+                                        for (int j = i; j < aOperators.Count - 1; j++)
+                                        {
+                                            aOperators[j] = aOperators[j + 1];
+                                            aNumbers[j + 1] = aNumbers[j + 2];
+                                        }
+                                        aNumbers.RemoveAt(aNumbers.Count - 1);
+                                        aOperators.RemoveAt(aOperators.Count - 1);
+                                        i--;
+                                    }
+                                    catch (OverflowException)
+                                    {
+                                        throw;
+                                    }
+                                    break;
+                                default:
+                                    throw new ArgumentException("Incorrect operator. Only + and - allowed");
+                            }
+                        }
+                        if (aNumbers.Count == 1)
+                        {
+                            return aNumbers[0];
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Argument Exception. Something goes wrong");
+                        }
+                    };
+                    var answer = myHandler(numbers, operators);
+                    Console.WriteLine(answer);
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("Solution maked with lambda(you can use only *, /)");
+            while (true)
+            {
+                try
+                {
+                    var input = Calculator.ReadInput();
+                    Calculator.ParseInput(input, out List<int> numbers, out List<char> operators);
+                    myHandler = (List<int> aNumbers, List<char> aOperators) =>
+                    {
+                        for (int i = 0; i < aOperators.Count; i++)
+                        {
+                            switch (aOperators[i])
+                            {
+                                case '*':
+                                    try
+                                    {
+                                        checked
+                                        {
+                                            aNumbers[i] = aNumbers[i] + aNumbers[i + 1];
+                                        }
+                                        for (int j = i; j < aOperators.Count - 1; j++)
+                                        {
+                                            aOperators[j] = aOperators[j + 1];
+                                            aNumbers[j + 1] = aNumbers[j + 2];
+                                        }
+                                        aNumbers.RemoveAt(aNumbers.Count - 1);
+                                        aOperators.RemoveAt(aOperators.Count - 1);
+                                        i--;
+                                    }
+                                    catch (OverflowException)
+                                    {
+                                        throw;
+                                    }
+                                    break;
+                                case '/':
+                                    try
+                                    {
+                                        checked
+                                        {
+                                            aNumbers[i] = aNumbers[i] / aNumbers[i + 1];
+                                        }
+                                        for (int j = i; j < aOperators.Count - 1; j++)
+                                        {
+                                            aOperators[j] = aOperators[j + 1];
+                                            aNumbers[j + 1] = aNumbers[j + 2];
+                                        }
+                                        aNumbers.RemoveAt(aNumbers.Count - 1);
+                                        aOperators.RemoveAt(aOperators.Count - 1);
+                                        i--;
+                                    }
+                                    catch (DivideByZeroException)
+                                    {
+                                        throw;
+                                    }
+                                    break;
+                                default:
+                                    throw new ArgumentException("Incorrect operator. Only * and / allowed");
+                            }
+                        }
+                        if (aNumbers.Count == 1)
+                        {
+                            return aNumbers[0];
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Argument Exception. Something goes wrong");
+                        }
+                    };
+                    var answer = myHandler(numbers, operators);
+                    Console.WriteLine(answer);
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
